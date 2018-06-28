9d1cdd4027e95aed3e7d4d77d37fb269a8e63876
Russell Smith <ukd1@users.noreply.github.com>
2018-06-27 21:42:57 -0700

    Add a more detailed version, for displaying issue in main pane

---
+++ src/components/github-issue/github-issue.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Debug from 'debug'
+
+import ContentTypes from '../../content-types'
+
+const log = Debug('pushpin:github-issue')
+
+export default class GithubIssue extends React.PureComponent {
+  static propTypes = {
+    docId: PropTypes.string.isRequired
+  }
+
+  static initializeDocument = (urlDoc, { stuff = '' }) => {
+    // don't need this yet
+  }
+
+  static minWidth = 9
+  static minHeight = 9
+  static defaultWidth = 12
+  // static defaultHeight = 18
+  static maxWidth = 24
+  static maxHeight = 32
+
+  componentWillMount = () => this.refreshHandle(this.props.docId)
+  componentWillUnmount = () => this.handle.release()
+  componentDidUpdate = (prevProps, prevState, snapshot) => {
+    if (prevProps.docId !== this.props.docId) {
+      this.refreshHandle(this.props.docId)
+    }
+  }
+  refreshHandle = (docId) => {
+    if (this.handle) {
+      this.handle.release()
+    }
+    this.handle = window.hm.openHandle(docId)
+    this.handle.onChange(this.onChange)
+  }
+
+  onChange = (doc) => {
+    this.setState({ ...doc })
+  }
+
+  render = () => {
+    const { contents } = this.state
+    if (!contents) {
+      return (
+        <div style={css.issueCard}>
+          Loading
+        </div>
+      )
+    }
+    return (
+      <div style={css.issueCard}>
+        <h1>{contents.title}</h1>
+        <small>#{contents.number} {contents.state}ed at {contents.created_at}</small>
+        <p>{contents.body}</p>
+        <ul>
+          <li>this will show {contents.comments} when it's complete...</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+ContentTypes.register({
+  component: GithubIssue,
+  type: 'github-issue',
+  name: 'GitHub Issue',
+  icon: 'github',
+  resizable: true
+})
+
+const css = {
+  issueCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    boxSizing: 'border-box',
+    overflow: 'auto',
+    position: 'relative',
+    padding: 12,
+    flex: '1 1 auto',
+    border: '1px solid var(--colorPaleGrey)'
+  },
+}
